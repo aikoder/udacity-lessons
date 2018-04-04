784881f68c1c3b6e5cf4bb2ba00a32e6c9730918
Gavin Huang - RGavin.HuangD <Gavin.Huang@aztech.com>
2018-04-04 18:13:45 +0800

    add get batch function

--- p6.p7.cats.vs.dogs/input_data.py
+++ p6.p7.cats.vs.dogs/input_data.py
@@ -5,11 +5,11 @@ Created on Wed Apr  4 15:17:49 2018
 @author: Gavin.Huang
 """
 
-#import tensorflow as tf
+import tensorflow as tf
 import numpy as np
 import os
 
-
+#%%
 # image size
 image_width = 208
 image_height = 208
@@ -52,6 +52,78 @@ def get_files(file_dir):
     return image_list, label_list
     
 
+#%%
+def get_batch(image, label, image_W, image_H, batch_size, capacity):
+    '''
+    Args:
+        image: list type
+        label: list type
+        image_W: image width
+        image_H: image height
+        batch_size: batch size
+        capacity: the maximum elements of queue
+    Returns:
+        image_batch: 4D tensor [batch_size, width, height, 3], dtype=tf.float32
+        label_batch: 1D tensor [batch_size], dtype=tf.int32
+    '''
+    image = tf.cast(image, tf.string)
+    label = tf.cast(label, tf.int32)
+    
+    # make an input queue
+    input_queue = tf.train.slice_input_producer([image, label])
+    
+    label = input_queue[1]
+    image_contents = tf.read_file(input_queue[0])
+    image = tf.image.decode_jpeg(image_contents, channels=3)
+    
+    image = tf.image.resize_image_with_crop_or_pad(image, image_W, image_H)
+    image = tf.image.per_image_standardization(image)
+    image_batch, label_batch = tf.train.batch([image, label],
+                                              batch_size=batch_size,
+                                              num_threads=64,
+                                              capacity=capacity)
+    label_batch = tf.reshape(label_batch, [batch_size])
+    
+    return image_batch, label_batch
 
 
+#%%
+# test
+#import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
+
+BATCH_SIZE = 2
+CAPACITY = 128
+image_width = 208
+image_height = 208
+
+print('image size: %dx%d' % (image_width, image_height))
+
+image_list, label_list = get_files(TRAIN_DIR)
+image_batch, label_batch = get_batch(image_list, label_list, image_width, image_height, BATCH_SIZE, CAPACITY)
+
+print(image_batch)
+print(label_batch)
+
+with tf.Session() as sess:
+    i = 0
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coord)
+    try:
+        while not coord.should_stop() and (i < 1):
+            img, label = sess.run([image_batch, label_batch])
+            
+            for j in np.arange(BATCH_SIZE):
+                print('label: %d' % label[j])
+                plt.imshow(img[j, :, :, :])
+                plt.show()
+            i += 1
+            
+    except tf.errors.OutOfRangeError:
+        print('Done!')
+    finally:
+        coord.request_stop()
+    coord.join(threads)
+    
+#####
